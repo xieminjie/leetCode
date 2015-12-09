3c7c6413e995000132bdcb956cd3a97792b99208
xieminjie <xieminjiexmj@gmail.com>
2015-12-09 14:24:21 +0800

    update

--- src/leetCode/Solution.java
+++ src/leetCode/Solution.java
@@ -5,5 +5,39 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class Solution {
-	
+	public String longestCommonPrefix(String[] strs) {
+		int maxCount = Integer.MAX_VALUE;
+		int index = 0;
+		String shortestStr = "";
+		String prefix = "";
+		for (int i = 0; i < strs.length; i++) {
+			String str = strs[i];
+			if (str.length() < maxCount) {
+				maxCount = str.length();
+				shortestStr = str;
+			}
+		}
+		boolean flag = true;
+		while(flag==true&& index<maxCount){
+			int n=0;
+			for(int j=0;j<strs.length;j++){
+				if(shortestStr.charAt(index)==strs[j].charAt(index)){
+					n++;
+				}else{
+					break;
+				}
+			}
+			if(n==strs.length){
+				index++;
+			}else{
+				flag = false;
+			}
+		}
+		if(shortestStr.length()>0){
+			prefix = shortestStr.substring(0,index);
+		}else{
+			prefix = "";
+		}
+		return prefix;
+	}
 }
--- src/leetCode/Test.java
+++ src/leetCode/Test.java
@@ -4,9 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int test = 1;
+		String[] strs = {};
 		Solution solution = new Solution();
-		System.out.println(solution.reverse(test));
+		System.out.println(solution.longestCommonPrefix(strs));
 	}
 
 }

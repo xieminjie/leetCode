e6ec11b630eaccbb3ea68ecb5931c3d4d1330057
xieminjie <xieminjiexmj@gmail.com>
2015-12-30 13:56:57 +1100

    update

--- src/leetCode/Test.java
+++ src/leetCode/Test.java
@@ -1,7 +1,8 @@
 package leetCode;
 
-import java.util.HashMap;
+
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
@@ -9,8 +10,33 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] array = {3};
+		//int[] array = {3};
+
+		ListNode li1 = new ListNode(2);
+		ListNode li2 = new ListNode(4);
+		ListNode li3 = new ListNode(6);
+		ListNode li4 = new ListNode(10);
+		ListNode li5 = new ListNode(12);
+		
+		ListNode li6 = new ListNode(3);
+		ListNode li7 = new ListNode(5);
+		ListNode li8 = new ListNode(9);
+		ListNode li9 = new ListNode(10);
+		li1.next = li2;
+		li2.next = li3;
+		li3.next = li4;
+		li4.next = li5;
+		
+		li6.next = li7;
+		li7.next = li8;
+		li8.next = li9;
 		Solution solution = new Solution();
-		System.out.println(solution.removeDuplicates(array));
+		ListNode listnode = solution.mergeTwoLists(li1,li6);
+		for(int i=0;i<9;i++){
+			System.out.println(listnode.val);
+			listnode = listnode.next;	
+		}
+		
+	
 	}
 }

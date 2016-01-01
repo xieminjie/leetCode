7e215b94ef5aa363806e45d4b3bdef921a6c3cbb
xieminjie <xieminjiexmj@gmail.com>
2016-01-01 11:37:12 +1100

    update

--- src/leetCode/Solution.java
+++ src/leetCode/Solution.java
@@ -2,11 +2,10 @@ package leetCode;
 
 public class Solution {
 	public int strStr(String haystack, String needle) {
-        int pointer = 0;
         if(haystack.length()==0||needle.length()==0){
         	return -1;
         }
-        for(int i=0;i<haystack.length()-needle.length();i++){
+        for(int i=0;i<haystack.length();i++){
         	for(int j=0;j<needle.length();j++){
         		if(needle.charAt(j)==haystack.charAt(i)){
         			j++;
@@ -17,6 +16,6 @@ public class Solution {
         		}
         	}
         }
-        return pointer;
+        return -1;
     }
 }
--- src/leetCode/Test.java
+++ src/leetCode/Test.java
@@ -11,7 +11,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution solution = new Solution();
-		System.out.println(solution.strStr("I am a good man","good"));
+		System.out.println(solution.strStr("I am a good man","am"));
 		
 	
 	}

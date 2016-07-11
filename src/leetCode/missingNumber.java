1ad14bc0aa3b7599fb316473c43c45ffc9ca4830
XieMinjie <xieminjie@XieMinjies-MacBook-Pro.local>
2016-07-11 12:18:06 +1000

    Add missingNumber solution

--- src/leetCode/missingNumber.java
+++ src/leetCode/missingNumber.java
@@ -1,17 +1,8 @@
 package leetCode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
-public class Solution {
+public class missingNumber {
 	 public int missingNumber(int[] nums) {
 			Arrays.sort(nums);
 			if(nums.length==1&&nums[0]==0){
@@ -29,5 +20,5 @@ public class Solution {
 	        }
 	        
 	        return count;
-	    }
+	 }
 }
